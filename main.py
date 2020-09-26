763970458c5a93b3cfe9e59c0031338423c8fe07
wangchengzhen <210087652@qq.com>
2020-09-27 00:13:22 +0800

    color and reverse

--- main.py
+++ main.py
@@ -33,11 +33,33 @@ def cpp(x):
 	print (l)
 
 
+def listDemo():
+	print(f'Reverse_Output:')
+	letterList = ["aaa", "bbb", "ccc", "ddd", "eee", "fff"]
+	print(f'ERROR:', letterList)
+	for i in range(0, int(len(letterList) / 2)):
+		letterList[i], letterList[-1 - i] = letterList[-1 - i], letterList[i]
+	print("AFTER", letterList)
+
+def color():
+	class manyColor:
+		HEADER = '\033[95m'
+		OKBLUE = '\033[94m'
+		OKGREEN = '\033[92m'
+		WARNING = '\033[93m'
+		FAIL = '\033[91m'
+		ENDC = '\033[0m'
+		BOLD = '\033[1m'
+		UNDERLINE = '\033[4m'
+	print(manyColor.WARNING,"警告的颜色字体?",manyColor.ENDC)
+	print(manyColor.BOLD,'HEADER',manyColor.ENDC)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     ##printDiamond()
-    cpp(8)
+    ##cpp(8)
+	##listDemo()
+	color()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
